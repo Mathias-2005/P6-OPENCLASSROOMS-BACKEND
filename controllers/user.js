59a55ce057ffc912a0dbfb2d52ecfa1594cf6353
Mathias <mathiasmaillydepinho@gmail.com>
2026-01-22 17:32:48 +0100

    add function login

--- controllers/user.js
+++ controllers/user.js
@@ -9,12 +9,29 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' })) 
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error })); // ERREUR SERVEUR
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+            }
+            bcrypt.compare(req.body.password, user.password) // COMPARE LE MDP 'STRING' AVEC UN HASH  
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
--- models/User.js
+++ models/User.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator'); // PACKAGE UNIQUE VALIDATEUR 
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true }, // UNIQUE PERMET DE S'ASSURER D'AVOIR UN SEUL ET UNIQUE MDP/EMAIL VALIDE
   password: { type: String, required: true }
 });
-
-// PERMET DE S'ASSURER D'AVOIR UN SEUL ET UNIQUE MDP/EMAIL VALIDE
-userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
