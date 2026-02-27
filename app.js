aa5e1ab74a73e17d276b1d4078749a9e8356a97e
Mathias <mathiasmaillydepinho@gmail.com>
2026-02-27 17:28:23 +0100

    add cors

--- app.js
+++ app.js
@@ -13,10 +13,18 @@ const app = express();
 
 // CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://mon-vieux-grimoire-frontend.onrender.com'
+  ];
+  
+  if (allowedOrigins.includes(req.headers.origin)) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
 });
 
 app.use(express.json());
